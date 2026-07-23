429d29d5a98d736b41e5183cc205b2ca44050131
Blessing <babumere04@gmail.com>
2026-07-23 03:25:05 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -110,6 +110,11 @@ impl Tag {
             Self::Other(s) => s,
         }
     }
+#[derive(Clone)]
+struct AppState {
+    movie_directory: PathBuf,
+    port: u16,
+}
     // Validate directory
     if !movie_directory.exists() {
         eprintln!("\x1b[1;31mError:\x1b[0m Directory '{}' does not exist.", raw_dir);
