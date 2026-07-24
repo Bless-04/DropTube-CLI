729993c71e4aef08226cea7c09609704ff244155
Blessing <babumere04@gmail.com>
2026-07-23 22:57:16 -0400

    moved to public

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,11 @@ use std::path::{Path as StdPath, PathBuf};
 use std::sync::Arc;
 use std::time::SystemTime;
 use tokio::sync::RwLock;
-use tower_http::services::ServeDir;
-use tower_http::catch_panic::CatchPanicLayer;
-use tower_http::services::ServeDir;
-use config::cli_flag::CliFlag;
 use ui::tailwind;
+
 pub mod config;
 pub mod ui;
+pub mod routes;
 
 // Embed the HTML & JS template at compile time
 const INDEX_HTML_TEMPLATE: &str = include_str!("website/index.html");
