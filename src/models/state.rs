720c0551f8887b9a0cf66f3e22ceac32eb67cc52
Blessing <babumere04@gmail.com>
2026-07-24 15:08:42 -0400

    Create state.rs

---
+++ src/models/state.rs
@@ -0,0 +1,18 @@
+use crate::models::video::VideoFile;
+use serde::Deserialize;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub movie_directory: PathBuf,
+    pub port: u16,
+    pub recurse: bool,
+    pub index_cache: Arc<RwLock<Vec<VideoFile>>>,
+}
+
+#[derive(Deserialize)]
+pub struct HomeQuery {
+    pub v: Option<String>,
+}
