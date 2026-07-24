7256ee47ae290e51b9a77ce72b7f88870732cca2
Blessing <babumere04@gmail.com>
2026-07-23 22:26:06 -0400

    Update mod.rs

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,7 +4,7 @@ use tower_http::catch_panic::CatchPanicLayer;
 use tower_http::services::ServeDir;
 use crate::{explorer_path_handler, explorer_root_handler, home_page_handler, refresh_index_handler, AppState};
 
-pub fn create_router(state: AppState) -> Router {
+pub(crate) fn create_router(state: AppState) -> Router {
     Router::new()
         .route("/", get(home_page_handler))
         .nest_service("/public", ServeDir::new("public"))
