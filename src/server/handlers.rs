0fa877eecda608279cf9e768f29fd3400a97d026
Blessing <babumere04@gmail.com>
2026-07-26 22:11:52 -0400

    Update handlers.rs

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -2,5 +2,6 @@ pub async fn refresh_index_handler(State(state): State<AppState>) -> StatusCode 
     let dir = state.movie_directory.clone();
     let recurse = state.recurse;
     let cache_clone = state.index_cache.clone();
+pub async fn home_page_handler(
     Html(full_html)
 }
