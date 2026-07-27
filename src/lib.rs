e9e34218f6f021fda5585ffb7bd7770932f94913
Blessing <babumere04@gmail.com>
2026-07-27 00:26:39 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+//#![deny(missing_docs)] todo when working as expected impl this
+
+/// crate for config 
 pub mod config;
+
+/// crate for stateful models 
 pub mod models;
 pub mod server;
 pub mod utils;
