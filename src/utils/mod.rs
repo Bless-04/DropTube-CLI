c8d3e94a6de641d204cf82211402b4cc7654315e
Blessing <babumere04@gmail.com>
2026-07-27 01:45:41 -0400

    Update mod.rs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,6 @@
+/// holds functions that print display to the user through the Command Line Interface
+pub mod display;
 pub mod scanner;
+
+/// contains functions related to interacting with tailwind
 pub mod tailwind;
