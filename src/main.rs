7e056ee15d667747238f6dd4da3bbef438a85edf
Blessing <babumere04@gmail.com>
2026-07-29 19:15:50 -0400

    Update main.rs
    using unwrap or else for signal handlers because .expect will force shutdown and that shouldnt happen for ctrl c or terminate not being handled gracefully

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,13 @@ async fn shutdown_signal() {
     let ctrl_c = async {
         tokio::signal::ctrl_c()
             .await
-            .expect("failed to install Ctrl+C handler");
+            .unwrap_or_else(|e| error!("{}", e));
     };
 
     #[cfg(unix)]
     let terminate = async {
         tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
+            .unwrap_or_else(|e| error!("{}", e))
             .recv()
             .await;
     };
