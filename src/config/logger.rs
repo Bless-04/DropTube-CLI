7335f20be90d2c7e81955b22f591577a556be8df
Blessing <babumere04@gmail.com>
2026-07-29 22:55:57 -0400

    Update logger.rs

--- src/config/logger.rs
+++ src/config/logger.rs
@@ -1,8 +1,10 @@
 use log::Level;
+use stderrlog::ColorChoice;
 
 pub fn create_log(level: Level) -> Result<(), log::SetLoggerError> {
     stderrlog::new()
         .module(stringify!(droptube))
         .verbosity(level)
+        .color(ColorChoice::Auto)
         .init()
 }
