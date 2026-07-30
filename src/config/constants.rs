65cea1947528e98bb110e91d45b88da0a6ab18c5
Blessing <babumere04@gmail.com>
2026-07-29 23:40:34 -0400

    Update constants.rs

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -1,8 +1,10 @@
-
-//impl Default for CliArgs {} todo for when CliArgs is added
-
 // Default Port for server
 pub const DEFAULT_PORT: u16 = 8081;
+
+/// `\r` (Carriage Return): Moves the blinking cursor back to the absolute beginning (column 0) of
+/// the current line. `\x1b[2K` (ANSI Erase Line): Wipes out all the text on the current line,
+/// regardless of where the cursor is positioned.
+pub const CLEAR_LINE_ANSI: &str = "\r\x1b[2K";
 
 // The file path of the html page
 pub const HTML_SOURCE: &str = include_str!("../../public/index.html");
