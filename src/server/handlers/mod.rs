f5ab60f8a4b3c34db40d7ca77a7618e86741f96c
Blessing <babumere04@gmail.com>
2026-08-08 13:26:20 -0400

    Update mod.rs

--- src/server/handlers/mod.rs
+++ src/server/handlers/mod.rs
@@ -1,4 +1,28 @@
 pub mod explorer;
 pub mod home;
 pub mod scan;
+
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use log::error;
+
+pub struct TemplateError(pub askama::Error);
+
+impl IntoResponse for TemplateError {
+    fn into_response(self) -> Response {
+        error!("Template rendering error: {}", self.0);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Internal Server Error",
+        )
+            .into_response()
+    }
+}
+
+impl From<askama::Error> for TemplateError {
+    fn from(err: askama::Error) -> Self {
+        TemplateError(err)
+    }
+}
