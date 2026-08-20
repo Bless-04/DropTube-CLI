9449d4c0952771c6cf9beace5ef185cf787c2453
Blessing <babumere04@gmail.com>
2026-08-20 05:39:51 -0400

    Update assets.rs

--- src/server/handlers/assets.rs
+++ src/server/handlers/assets.rs
@@ -4,6 +4,21 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use rust_embed::RustEmbed;
+
 #[derive(RustEmbed)]
 #[folder = "public/"]
 struct Assets;
+
+pub async fn static_handler(uri: axum::http::Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/');
+    if let Some(content) = Assets::get(path) {
+        let mime = mime_guess::from_path(path).first_or_octet_stream();
+        Response::builder()
+            .header(header::CONTENT_TYPE, mime.as_ref())
+            .body(Body::from(content.data))
+            .unwrap_or_else(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())
+            .into_response()
+    } else {
+        StatusCode::NOT_FOUND.into_response() // case when its not in the deployed folder
+    }
+}
