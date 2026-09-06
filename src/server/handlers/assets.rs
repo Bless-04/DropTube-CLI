daddba76622f49b2ce7a548b2f37892fc7d855b2
Blessing <babumere04@gmail.com>
2026-09-06 13:18:18 -0400

    Update assets.rs
    added assets tests

--- src/server/handlers/assets.rs
+++ src/server/handlers/assets.rs
@@ -22,3 +22,17 @@ pub async fn static_handler(uri: axum::http::Uri) -> impl IntoResponse {
         StatusCode::NOT_FOUND.into_response() // case when its not in the deployed folder
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{body::to_bytes, extract::Path};
+
+    #[tokio::test]
+    async fn serves_application_assets_with_content_types() {
+        let response = static_handler(Path("index.js".to_owned()))
+            .await
+            .into_response();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert!(
+            response.headers()[header::CONTENT_TYPE]
