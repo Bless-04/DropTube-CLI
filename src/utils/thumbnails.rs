da76f8717a0793dccd01eeacd096b9240a6f387c
Blessing <babumere04@gmail.com>
2026-09-08 12:44:40 -0400

    Update thumbnails.rs

--- src/utils/thumbnails.rs
+++ src/utils/thumbnails.rs
@@ -15,6 +15,30 @@ pub struct ThumbnailGenerator {
 impl ThumbnailGenerator {
     /// The generated path for the thumbnails made by ffmpeg
     pub const GENERATED_PATH: &str = ".droptube-thumbnails";
+
+    /// Args to generate the thumbnail
+    pub const GENERATION_ARGS: [&'static str; 19] = [
+        "-map",
+        "0:v:0",
+        "-an",
+        "-sn",
+        "-filter_threads",
+        "1",
+        "-vf",
+        "scale=480:270:force_original_aspect_ratio=decrease,pad=480:270:(ow-iw)/2:(oh-ih)/2,setsar=1,thumbnail=30",
+        "-frames:v",
+        "1",
+        "-c:v",
+        "mjpeg",
+        "-threads",
+        "1",
+        "-q:v",
+        "3",
+        "-f",
+        "image2pipe",
+        "pipe:1",
+    ];
+
     /// Checks that the executable starts and identifies itself as FFmpeg.
     /// Returns an error if it is absent, unusable, or takes more than five seconds.
     pub async fn new(executable: PathBuf) -> io::Result<Self> {
